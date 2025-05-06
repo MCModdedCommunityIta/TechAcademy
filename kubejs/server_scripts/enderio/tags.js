cf34ba46872eacbda7accbbaed1b1ac7fd4d814f
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-05-07 01:20:36 +0200

    Added some Missing Head from loot fabircator #8

---
+++ kubejs/server_scripts/enderio/tags.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', event => {
+  event.add('minecraft:skulls', 'enderio:enderman_head')
+})
+
+ServerEvents.tags('block', event => {
+  event.add('c:skulls', 'enderio:enderman_head')
+})
