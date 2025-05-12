aec951e4aa61c47772025f00d5c8686ac8de7624
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-05-13 01:49:47 +0200

    other fluids

--- kubejs/server_scripts/oritech/fluidUnification.js
+++ kubejs/server_scripts/oritech/fluidUnification.js
@@ -12,5 +12,74 @@ ServerEvents.recipes((event) => {
     },
     processTime: 60,
   });
+  addCentrifuge(event, {
+    fluidInput: {
+      fluid: "oritech:still_oil",
+      amount: 1000,
+    },
+    fluidOutput: {
+      fluid: "pneumaticcraft:oil",
+      amount: 1000,
+    },
+    processTime: 60,
+  });
+  addCentrifuge(event, {
+    fluidInput: {
+      fluid: "bigreactors:steam",
+      amount: 1000,
+    },
+    fluidOutput: {
+      fluid: "oritech:still_steam",
+      amount: 1000,
+    },
+    processTime: 60,
+  });
+  addCentrifuge(event, {
+    fluidInput: {
+      fluid: "mekanism:steam",
+      amount: 1000,
+    },
+    fluidOutput: {
+      fluid: "oritech:still_steam",
+      amount: 1000,
+    },
+    processTime: 60,
+  });
+  addCentrifuge(event, {
+    fluidInput: {
+      fluid: "oritech:still_steam",
+      amount: 1000,
+    },
+    fluidOutput: {
+      fluid: "bigreactors:steam",
+      amount: 1000,
+    },
+    itemsInput: [{
+      item: "bigreactors:graphite_ingot",
+    }],
+    itemsOutput: [{
+      id: "bigreactors:graphite_ingot",
+      count: 1,
+    }],
+    processTime: 60,
+  });
+  addCentrifuge(event, {
+    fluidInput: {
+      fluid: "oritech:still_steam",
+      amount: 1000,
+    },
+    fluidOutput: {
+      fluid: "mekanism:steam",
+      amount: 1000,
+    },
+    itemsInput: [{
+      item: "mekanism:ingot_osmium",
+    }],
+    itemsOutput: [{
+      id: "mekanism:ingot_osmium",
+      count: 1,
+    }],
+    processTime: 60,
+  });
 
 });
