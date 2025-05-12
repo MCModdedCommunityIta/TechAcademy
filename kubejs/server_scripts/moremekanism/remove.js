2a9ae0161da33d975460123486bad847df5e43df
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-05-13 01:50:53 +0200

    unsued more mekanism removing (no slurry, missing kubejs function)

--- kubejs/server_scripts/moremekanism/remove.js
+++ kubejs/server_scripts/moremekanism/remove.js
@@ -1,34 +1,94 @@
-// function recipeExist(id) {
-//   Item.of(id).forEachRecipe((recipe) => {
-//     return true
+const itemsToRemove_mmp = [];
+
+
+const materialToRemove_mmp = [
+  "apatite",
+  "azure_silver",
+  "bismuth",
+  "bort",
+  "cinnabar",
+  "cobalt",
+  "crimson_iron",
+  "desh",
+  "dilithium",
+  "draconium",
+  "electrotine",
+  "green_sapphire",
+  "iridium",
+  "lithium",
+  "niter",
+  "peridot",
+  "ruby",
+  "sapphire",
+  "ostrum",
+  "calorite",
+  "sulfur",
+  "titanium",
+  "tungsten"
+];
+
+
+
+
+ServerEvents.recipes((event) => { 
+  let outputs = [];
+  materialToRemove_mmp.forEach((mmp) => {
+    outputs.push({"output": `moremekanismprocessing:crystal_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:shard_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:clump_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:dirty_dust_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:dust_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:${mmp}_ingot`});
+    outputs.push({"output": `moremekanismprocessing:${mmp}_nugget`});
+    outputs.push({"output": `moremekanismprocessing:gem_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:clean_${mmp}`});
+    outputs.push({"output": `moremekanismprocessing:dirty_${mmp}`});
+  })
+  outputs.forEach((output) => {
+    try {
+      event.remove(output);
+    } catch (error) {
+      
+    }
+  })
+});
+
+
+RecipeViewerEvents.removeEntriesCompletely('item', event => {
+  let outputs = [];
+  materialToRemove_mmp.forEach((mmp) => {
+    outputs.push(`moremekanismprocessing:crystal_${mmp}`);
+    outputs.push(`moremekanismprocessing:shard_${mmp}`);
+    outputs.push(`moremekanismprocessing:clump_${mmp}`);
+    outputs.push(`moremekanismprocessing:dirty_dust_${mmp}`);
+    outputs.push(`moremekanismprocessing:dust_${mmp}`);
+    outputs.push(`moremekanismprocessing:${mmp}_ingot`);
+    outputs.push(`moremekanismprocessing:${mmp}_nugget`);
+    outputs.push(`moremekanismprocessing:gem_${mmp}`);
+  })
+  outputs.forEach((output) => {
+    try {
+      event.remove(output);
+    } catch (error) {
+      
+    }
+  })
+})
+
+// RecipeViewerEvents.removeEntriesCompletely('gas', event => {
+//   let outputs = [];
+//   materialToRemove_mmp.forEach((mmp) => {
+//     outputs.push(`moremekanismprocessing:clean_${mmp}`);
+//     outputs.push(`moremekanismprocessing:dirty_${mmp}`);
 //   })
-//   return false
-// }
-
-
-// if (recipeExist("moremekanismprocessing:crystal_aluminum")) {
-//   console.log("che belli i cazzi")
-// }
-
-// {
-//   "neoforge:conditions": [
-//     {
-//       "type": "moremekanismprocessing:processing_level",
-//       "materialType": "aluminum",
-//       "requireLevel": 5
+//   outputs.forEach((output) => {
+//     try {
+//       event.remove(output);
+//     } catch (error) {
+      
 //     }
-//   ],
-//   "type": "mekanism:washing",
-//   "chemical_input": {
-//     "amount": 1,
-//     "chemical": "moremekanismprocessing:dirty_aluminum"
-//   },
-//   "fluid_input": {
-//     "amount": 5,
-//     "fluid": "minecraft:water"
-//   },
-//   "output": {
-//     "amount": 1,
-//     "id": "moremekanismprocessing:clean_aluminum"
-//   }
-// }
+//   })
+// })
+
+
+
