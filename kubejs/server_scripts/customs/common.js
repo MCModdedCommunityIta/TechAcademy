dc3ed6d4c0e0baa2029f6137c41b197da6b18314
DevDyna <andrearisorti2003@gmail.com>
2025-05-21 22:32:03 +0200

    Update common.js

--- kubejs/server_scripts/customs/common.js
+++ kubejs/server_scripts/customs/common.js
@@ -1,23 +1,23 @@
-//database of all time operations  
+//database of all time operations
 const time = {
-    seconds: 20,
-    minutes: 20 * 60,
-    hours: 20 * 60 * 60,
-    days: 20 * 60 * 60 * 24,
-    weeks: 20 * 60 * 60 * 24 * 7,
-    months: 20 * 60 * 60 * 24 * 30,
-    years: 20 * 60 * 60 * 24 * 365,
-  };
+  seconds: 20,
+  minutes: 20 * 60,
+  hours: 20 * 60 * 60,
+  days: 20 * 60 * 60 * 24,
+  weeks: 20 * 60 * 60 * 24 * 7,
+  months: 20 * 60 * 60 * 24 * 30,
+  years: 20 * 60 * 60 * 24 * 365,
+};
 
 /**
  * Converts a given number of ticks into a specified time unit.
  *
  * @param {number} ticks - The number of ticks to be converted.
- * @param {string} outputTimeType - The desired time unit for conversion. 
- *        Can be one of the following: "seconds", "minutes", "hours", "days", 
- *        "weeks", "months", "years". If not specified or unrecognized, 
+ * @param {string} outputTimeType - The desired time unit for conversion.
+ *        Can be one of the following: "seconds", "minutes", "hours", "days",
+ *        "weeks", "months", "years". If not specified or unrecognized,
  *        returns the input ticks value.
- * @returns {number} - The equivalent time in the specified unit, or the 
+ * @returns {number} - The equivalent time in the specified unit, or the
  *          original ticks if the unit is unrecognized.
  */
 function ticksToTime(ticks, outputTimeType) {
