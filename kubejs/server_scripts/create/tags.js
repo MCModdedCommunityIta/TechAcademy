8f7faa8d1cd1b4efc17d22b14932c3d8e6a78ec2
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-05-23 01:43:04 +0200

    fix tags first load

--- kubejs/server_scripts/create/tags.js
+++ kubejs/server_scripts/create/tags.js
@@ -1,7 +1,9 @@
 ServerEvents.tags('item', event => {
-  // completamento assegnamento tag parazile della ori sulla create
-  Ingredient.of('#create:crushed_raw_materials').getItemIds().forEach((id) => {
-    let material = id.split('_').pop()
-    event.add(`c:clumps/${material}`, id)
-  })
+  // completamento assegnamento tag parziale della ori sulla create
+
+  getIdsByTagPre(event, "create:crushed_raw_materials").forEach((id) => {
+    let material = String(id).split('_').pop()
+    event.add(`c:clumps/${material}`, String(id))
+    });
+
 })
